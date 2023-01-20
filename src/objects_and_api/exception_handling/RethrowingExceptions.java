afca542305d2f5e6c358b241ed811aada815ff1f
Sandun Jayawardhana <99672222+SandunJay@users.noreply.github.com>
2023-01-21 00:26:20 +0530

    rethrow

--- src/objects_and_api/exception_handling/RethrowingExceptions.java
+++ src/objects_and_api/exception_handling/RethrowingExceptions.java
@@ -1,20 +1,21 @@
-package exceptions;
+package objects_and_api.exception_handling;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.Scanner;
 
 public class RethrowingExceptions {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         rethrowException();
     }
 
-    public static void rethrowException() {
+    public static void rethrowException() throws IOException{
         File file = new File("nonexistent/file.txt");
         file.createNewFile();
 
-        //Scanner fileReader = new Scanner(file);
+        Scanner fileReader = new Scanner(file);
     }
 }
